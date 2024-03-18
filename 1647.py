7ba3fb135235da02f8d788844a27542bb186658d
woojin <zaza0804@uos.ac.kr>
2024-03-18 16:17:54 +0900

    mst

--- 1647.py
+++ 1647.py
@@ -7,34 +7,35 @@ V,E=map(int,input().split())
 dist=[[] for i in range(V+1)]
 for _ in range(E):
     come=list(map(int,input().split()))
-    dist[come[0]].append([come[1],come[2]])
-    dist[come[1]].append([come[0],come[2]])
+    dist[come[0]].append((come[1],come[2]))
+    dist[come[1]].append((come[0],come[2]))
 
 start=1
-distance=[1000000]*(V+1)
-distance[1]=0
 check=0
 already=[False]*(V+1)
 answer=0
-queue=[[0,1]]
+queue=[(0,1)]
+big=0
 
-while queue:
-    check+=1
+for _ in range(V-1):
     small=heapq.heappop(queue)
     start=small[1]
     already[start]=True
     answer+=small[0]
-    
-    if check==V:
-        break
+    if big<small[0]:
+        big=small[0]
     
     for n,ndist in dist[start]:
         if not already[n]:
-            heapq.heappush(queue,[ndist,n])
+            heapq.heappush(queue,(ndist,n))
     while True:
         small=heapq.heappop(queue)
         if already[small[1]]==False:
             heapq.heappush(queue, small)
             break
-    
-print(answer)
+start=small[1]
+already[start]=True
+answer+=small[0]
+if big<small[0]:
+    big=small[0]
+print(answer-big)
